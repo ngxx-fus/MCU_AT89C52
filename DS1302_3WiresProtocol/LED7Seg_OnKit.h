e48ab0ba1ed6790f086940115968a3c4330f1e09
ngxx-fus <msnp@outlook.com.vn>
2024-09-30 19:55:55 +0700

    Add note to LED7Seg_OnKit.h

--- DS1302_3WiresProtocol/LED7Seg_OnKit.h
+++ DS1302_3WiresProtocol/LED7Seg_OnKit.h
@@ -2,6 +2,7 @@
 #ifndef _LED7SEG_ONKIT_H_
 #define _LED7SEG_ONKIT_H_
 // Thư viện cơ sở cho AT89C52
+#include "ThreeWiresProtocol.h"
 #include <REGX52.h>
 //---------- Macros -----------
 #ifndef elif
@@ -31,7 +32,9 @@ sbit GND2 = P2^4;
 const uint DIGIT_CODE[] = {0X3F, 0X06, 0X5B, 0X4F, 0X66, 0X6D,
                            0X7D, 0X07, 0X7F, 0X6F, 0X0, 0X1, 0x40, 0x8};
 
-uint DISP_Time = 14;//mili-sec
+uint LED[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+uint DISP_Time = 5;//mili-sec
 uint DISP_Freq = 24;//Hz
 
 void set_disp_freq(uint newDISP_Freq){
@@ -79,6 +82,23 @@ Hàm sẽ hiện thị số được định nghĩa trong DIGIT_CODE,
 */
 void digit_disp	(uint POS, uint DIGIT){
     led7seg_disp(POS, DIGIT_CODE[DIGIT]);
+}
+
+/*
+Hiển thị trong một GIÂY cả 8 LED7Seg.
+Nội dung hiển thị của LED thứ i tuỳ thuộc vào 
+giá trị chứa trong LED[i].
+*/
+void Disp8leds7seg(){
+    uint i = 0; 
+    uint j = 0;
+    set_disp_freq(DISP_Freq);
+    for(j = 1; j <= DISP_Freq; ++j){
+        for( int i =0; i < 7; ++i){
+            digit_disp(i, LED[i]);
+            delay_ms(DISP_Time);
+        }
+    }
 }
 
 #endif
